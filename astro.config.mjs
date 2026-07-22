85012d2dbf0e6fa479d7c143ac7581b04bf7ea1b
Trevor Davis <trevordavis74@gmail.com>
2026-07-22 19:46:16 -0400

    Update blog navigation and title for clarity; change "Market News" to "Local Market News"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://trevorphillipdavis.github.io',
+  site: 'https://arelystheamazingrealtor.com,
   base: '/arelysrealestatesite',
   integrations: [tailwind()],
 });
