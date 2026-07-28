5af9350577ab0235607ba6aa9ba84519c7f79b4b
Trevor Davis <trevordavis74@gmail.com>
2026-07-28 19:17:47 -0400

    chore: remove placeholder comment for deployed domain URL in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Replace with your actual deployed domain URL
   site: 'https://arelystheamazingrealtor.com',
   integrations: [
     tailwind(),
